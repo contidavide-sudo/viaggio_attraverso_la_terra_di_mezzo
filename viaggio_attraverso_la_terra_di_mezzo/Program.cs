e9390b43c8c23bee96d94f088f9537c7c6be41bc
contidavide-sudo <conti.davide@franchettisalviani.net>
2025-12-14 23:13:29 +0100

    prpseguimento lavoro

--- viaggio_attraverso_la_terra_di_mezzo/Program.cs
+++ viaggio_attraverso_la_terra_di_mezzo/Program.cs
@@ -9,7 +9,7 @@
             Console.WriteLine();
             Console.WriteLine("------------------------------------");
             Console.WriteLine("|1) Frodo: +5 punti vita            |");
-            Console.WriteLine("|2) Sam: +1 danno base              |");
+            Console.WriteLine("|2) Sam: +1 danno                   |");
             Console.WriteLine("|3) Merry: +1 probablità di fuga    |");
             Console.WriteLine("------------------------------------");
 
@@ -40,14 +40,39 @@
             Random lancio = new Random();
             return lancio.Next(1, 5);
         }
+
+        static int EventoCasuale()
+        {
+
+            Random lancio = new Random();
+            return lancio.Next(1, 4);
+
+        }
+
+        static int Combattimento(int puntiVita, int dannoBase, string[] inv)
+        {
+            int vitaNem = 8;
+
+            while (vitaNem > 0 || puntiVita > 0)
+            {
+                Console.WriteLine("Cosa vuoi utilizzare per combattere?");
+
+                for (int i = 0; i < inv.Length; i++) {
+
+                    Console.WriteLine("[" + inv[i] + "]");
+                
+                }
+              
+            }        
+        }
         static void Main(string[] args)
         {
-            int vita = 10, scelta = 0, avanzamento, indice = 0 ;
+            int vita = 10, danno = 2, scelta = 0, avanzamento, indice = 0, evento, sceltaNem;
             string personaggio = "";
 
             string[] Luogo = { "La Contea", "Bosco Atro", "Casa di Tom Bombadil", "Brea", "Colle Vento", "Gran Burrone", "Moria", "Lothlórien", "Fiume Anduin", "Amon Hen", "Fangorn", "Rohan", "Fosso di Helm", "Isengard", "Osgiliath", "Minas Morgul", "Minas Tirith", "Campi del Pelennor", "Cirith Ungol", "Monte Fato" };
 
-            string[] inventario = { "x", "x", "x", "x", "x" };
+            string[] inventario = { "Spada", "x", "x", "x", "x" };
 
             personaggio = SceltaPersonaggio(personaggio);
 
@@ -56,6 +81,10 @@
             if (personaggio == "Frodo")
             {
                 vita = vita + 5;
+            }
+            else if(personaggio == "Sam")
+            {
+                danno = danno + 1;
             }
 
             Console.WriteLine();
@@ -83,6 +112,31 @@
                     Console.WriteLine();
 
                     Console.WriteLine("Sei riuscito ad avanzare fino a " + Luogo[indice]);
+
+                    evento = EventoCasuale();
+
+                    if(evento == 1)
+                    {
+
+                        Console.WriteLine("Un Nazgul ti ha trovato e ti sta attaccando, vuoi ingaggiare un combattimento(1) o tentare la fuga(2)?"); 
+                        sceltaNem=Convert.ToInt32(Console.ReadLine());
+
+                        if(sceltaNem == 1)
+                        {
+
+                        }
+
+
+
+                    }
+                    else if(evento == 2)
+                    {
+                        Console.WriteLine("Durante il tuo viaggio hai incontrato ");
+                    } 
+                    else
+                    {
+                        Console.WriteLine("Hai trovato un oggetto");
+                    }
 
                 }else if(scelta == 2)
                 {
